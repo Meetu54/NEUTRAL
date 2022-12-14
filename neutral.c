3655839878b0e799234b04387e42f784099e4d5e
Meet Savaliya <89479235+Meetu54@users.noreply.github.com>
2022-12-14 04:51:14 -0800

    Add files via upload

---
+++ neutral.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main(){
+	
+	int number;
+	
+	printf("ENTER THE NUMBER : ");
+	scanf("%d",&number);
+	
+	if(number>0){
+		
+		printf("Number is Positive");
+	}
+	else if(number<0) {
+
+  	
+        printf("Number is Negative");
+}
+    
+	 else{
+	 	
+	    printf("Number is Neutral");
+	    
+	}
+	     
+}
